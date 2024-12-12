a5a2cd224302dd8482dd8ab90d9c5b4e098af385
Ian Yates <ian@iyates.co.uk>
2024-12-12 13:16:47 +0000

    Latest copy

--- pow.js/src/pow.js
+++ pow.js/src/pow.js
@@ -1,131 +1,137 @@
-// TODO: click binding, functions
-export default (function () {
+export default (() => {
+    const _regex = /\{\{\s*(!*\*?[\$\w]+(\.\*?[\$\w]+)*.*?)\s*\}\}/g
+
     function findChildTemplates(element) {
         element.removeAttribute('pow')
         const dom = element.content ?? element
-        return Array.from(dom.querySelectorAll('*[pow]:not([pow] [pow])'))
+        return [...dom.querySelectorAll('*[pow]:not([pow] [pow])')]
     }
 
-    function resolvePath(state, path) {
-        const parts = path?.split('.') ?? []
-        for (var i = 0; i < parts.length; ++i) {
-            const part = parts[i]
-            if (['*first', '*index', '*last', '*parent', '*path'].includes(part)) {
-                state = { data: state[part.substring(1)] }
-            } else if (part == '*index1') {
-                state = { data: state.index != null ? state.index + 1 : undefined }
-            } else if (part == '*true' || part == '*false') {
-                state = { data: part == '*true' }
-            } else if (part && state.data?.hasOwnProperty(part)) {
-                state = { data: state.data[part] }
-            } else if (part == 'length' && !Array.isArray(state.data)) {
-                state = { data: Object.keys(state.data).length }
-            } else if (part != '*data') {
-                return undefined
+    function consumeBinding(element, bindings = ['item', 'array', 'if', 'ifnot']) {
+        var result = {};
+        bindings.find(attr => {
+            const token = element.getAttribute(attr)
+            if (token != null) {
+                element.removeAttribute(attr)
+                return result = { attr, token }
             }
+        })
+        return result
+    }
+
+    function parseText(text, state, match) {
+        while (match = _regex.exec(text)) {
+            const value = resolveToken(match[1], state)
+            _regex.lastIndex = match.index + `${value}`.length
+            text = text.substring(0, match.index) + (value ?? '') + text.slice(match.index + match[0].length)
         }
-        return path ? state.data : undefined
+        return text
+    }
+
+    function resolveToken(token, state) {
+        try {
+            token = token[0] == '*' ? '$' + token.slice(1) : token
+            const args = Object.entries(!Array.isArray(state.$data) & typeof state.$data != 'string' ? { ...state.$data, ...state } : { ...state })
+            const value = (new Function(...args.map($ => $[0]), `return ${token}`))(...args.map($ => $[1]))
+            if (typeof value == 'function') {
+                const uufn = `_${Math.random().toString(36).slice(2)}`
+                window.$pow$[uufn] = (el) => value.call(el, state.$data, state.$root)
+                return `$pow$.${uufn}(this)`
+            }
+            return value
+        } catch (e) {
+            console.warn('Interpolation failed', { '*path': state.$path, token }, e)
+        }
     }
 
     function updateSiblingCondition(sibling, value) {
-        if (sibling?.hasAttribute('pow') && ['else-if', 'else-ifnot', 'else'].some($ => sibling.hasAttribute($))) {
-            if (value) {
+        if (sibling?.attributes['pow']) {
+            const { attr, token } = consumeBinding(sibling, ['else-if', 'else-ifnot', 'else'])
+            if (attr & value) {
                 sibling.remove()
                 return true
             }
-            sibling.setAttribute('if', sibling.getAttribute('else-if') || '*true')
-            sibling.setAttribute('ifnot', sibling.getAttribute('else-ifnot') || '*true')
-            sibling.removeAttribute('else-if')
-            sibling.removeAttribute('else-ifnot')
-            sibling.removeAttribute('else')
+            if (attr & attr != 'else') {
+                sibling.setAttribute(attr.slice(5), token)
+            }
         }
     }
 
-    function processElement(element, state, array = false) {
-        if (element.hasAttribute('if') || element.hasAttribute('ifnot')) {
-            const condition = element.getAttribute('if') || element.getAttribute('ifnot')
-            console.debug('if', state.path, condition)
-            const value = element.hasAttribute('if') ? !!resolvePath(state, condition) : !resolvePath(state, condition)
-            while (updateSiblingCondition(element.nextElementSibling, value));
-            if (!value) {
+    function processElement(element, state) {
+        const { attr, token } = consumeBinding(element)
+        const value = token && state ? resolveToken(token, state) : undefined
+        if (attr == 'if' | attr == 'ifnot') {
+            const isnot = attr != 'if' ? value : !value
+            while (updateSiblingCondition(element.nextElementSibling, !isnot));
+            if (isnot) {
                 element.remove()
                 return
             }
-        } else if (!array) {
-            const path = element.getAttribute('bind')
-            console.debug('bind', state.path, path)
+        } else if (attr == 'item') {
             state = {
-                path: state.path + '>' + (path || element.id || ''),
-                data: path ? resolvePath(state, path) : state.data,
-                parent: path ? state : state.parent
+                $path: `${state.$path}.${token || element.id || ''}`,
+                $data: token ? value : state.$data,
+                $parent: token ? state.$data : state.$parent,
+                $root: state.$root
             }
-            if (path && state.data === undefined) {
-                console.warn('Template binding without data', state.path)
-                return
+            if (token & state.$data === undefined) {
+                console.warn('Template binding without data', state.$path)
             }
-        }
-
-        if (state.index == null && element.hasAttribute('array') && typeof state.data == 'object') {
-            const array = state.data instanceof Array
-                ? state.data
-                : Object.entries(state.data).map(([k, v]) => ({ $key: k, $value: v }))
-            const html = element.innerHTML
-            for (var index = 0; index < array.length; ++index) {
-                element.innerHTML = html
-                processElement(element, {
-                    path: state.path + '[' + index + ']',
-                    index, first: index == 0, last: index == array.length - 1,
-                    data: array[index],
-                    parent: state
-                }, true)
+        } else if (attr == 'array' & typeof state.$data == 'object') {
+            const array = Array.isArray(value) ? value
+                : Object.entries(value).map(([k, v]) => ({ $key: k, $value: v }))
+            for (var $index = 0; $index < array.length; ++$index) {
+                const child = element.cloneNode(1)
+                element.parentNode.insertBefore(child, element)
+                processElement(child, {
+                    $path: `${state.$path}${token ? '.' + token : ''}[${$index}]`,
+                    $index, first: !$index, $last: $index > array.length - 2,
+                    $data: array[$index],
+                    $parent: state,
+                    $root: state.$root
+                })
             }
+            element.remove()
             return
         }
 
         const innerTemplates = findChildTemplates(element)
         for (const childTemplate of innerTemplates) {
-            if (childTemplate.parentNode) {
-                processElement(childTemplate, state)
-            }
+            processElement(childTemplate, state)
         }
 
-        const html = replacePlaceholders(element.innerHTML, state)
-        const range = document.createRange()
-        const target = (element instanceof HTMLTemplateElement ? element : element.childNodes[0]) ?? element
-        if (!target.parentNode) { console.warn('Bad parent', target); return }
-        range.setStartBefore(target)
-        range.insertNode(range.createContextualFragment(html))
-        while (target.nextSibling) {
-            target.nextSibling.remove()
+        for (const { name, value } of element.attributes) {
+            element.setAttribute(name, parseText(value, state))
         }
-        target.remove()
-        return range
-    }
-
-    const _regex = /\{\{\s*((?:\*)?[\$\w]+(?:\.(?:\*)?[\$\w]+)*)\s*\}\}/g
-    function replacePlaceholders(html, state) {
-        var match
-        while (match = _regex.exec(html)) {
-            const value = resolvePath(state, match[1])
-            if (value === undefined) {
-                console.warn('Placeholder not resolved', state.path, match[1])
-            } else {
-                _regex.lastIndex = match.index + value?.length
-            }
-            html = html.substring(0, match.index) + (value || '') + html.substring(match.index + match[0].length)
+        const html = parseText(element.innerHTML, state)
+        if (element instanceof HTMLTemplateElement) {
+            element.insertAdjacentHTML('afterend', html)
+            element.remove()
+        } else {
+            element.innerHTML = html
         }
-        return html
     }
 
     function bind(element) {
-        var range
+        const originalHTML = element.innerHTML
+        const attributes = [...(element.attributes || [])].map(({ name, value }) => [name, value])
         const binding = {
-            remove: () => range?.deleteContents
-        }
-        binding.apply = (data) => {
-            range?.surroundContents(element)
-            range = processElement(element, { path: '', data })
-            return binding
+            apply: (data) => {
+                if (element.$pow) {
+                    return console.warn('Binding already in progress')
+                }
+                element.$pow = 1
+                window.$pow$ = {}
+                element.innerHTML = originalHTML
+                for (const [name, value] of attributes) {
+                    element.setAttribute(name, value)
+                }
+                processElement(element, { $path: '*root', $data: data, $root: data })
+                delete element.$pow
+                binding.refresh = () => binding.apply(data)
+                return binding
+            },
+            refresh: () => { }
         }
         return binding
     }
