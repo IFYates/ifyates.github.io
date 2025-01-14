9183c642453ebd0636d6a55cba3298305231cae8
Ian Yates <ian@iyates.co.uk>
2025-01-14 15:05:18 +0000

    Updated documentation

--- pow.js/docs/nav.js
+++ pow.js/docs/nav.js
@@ -1,10 +1,5 @@
 import pow from '../src/pow.js'
 
-const mainBinding = pow.bind(document.getElementsByTagName('main')[0])
-window.refreshMain = () => {
-    mainBinding.refresh()
-}
-
 window.fn = (value) => typeof value == 'function' ? value() : value
 
 const nav = {
@@ -13,12 +8,7 @@ const nav = {
         this.href = 'javascript:void(0)'
         nav.current = item.id ?? item
         history.pushState(null, null, `?/${nav.current}/`)
-        refreshMain()
-        if (item.hash) {
-            location.hash = item.hash
-        } else {
-            window.scrollTo(0, 0)
-        }
+        location.hash = item.hash || nav.current
         return false
     },
     isCurrent: (a, b, c) => {
@@ -43,39 +33,31 @@ const nav = {
     ],
 
     pages: [
-        { id: 'home', name: 'Home', icon: 'fas fa-home' },
+        { id: 'home', name: 'Introduction', icon: 'fas fa-home' },
         { id: 'get-started', name: 'Getting started', icon: 'fas fa-graduation-cap' },
-        {
-            id: 'syntax',
-            name: 'Syntax',
-            icon: 'fas fa-code',
-            children: [
-                { id: 'syntax-binding', name: 'Bindings' },
-                { id: 'syntax-expressions', name: 'Expressions' },
-                { id: 'syntax-attributes', name: 'Attributes' },
-            ]
-        },
+        { id: 'syntax', name: 'Syntax', icon: 'fas fa-code' },
         {
             id: 'bindings',
             name: 'Bindings',
             icon: 'fas fa-link',
             children: [
-                { id: 'bindings-loops', name: 'array' },
-                { id: 'syntax-binding', hash: 'data', name: () => activeVersion >= 2.0 ? 'data' : 'item' },
+                { id: 'bindings-data', name: () => activeVersion >= 2.0 ? 'data' : 'item' },
                 { id: 'bindings-conditions', name: 'if / ifnot / else' },
+                { id: 'bindings-loops', name: 'array' },
+                { id: 'bindings-section', name: 'section', visible: () => activeVersion >= 3.3 },
+                { id: 'bindings-stop', hash: 'stop', name: 'stop', visible: () => activeVersion >= 1.4 },
                 { id: 'bindings-templates', name: 'template', visible: () => activeVersion >= 1.2 },
-                { id: 'syntax-binding', hash: 'stop', name: 'stop', visible: () => activeVersion >= 1.4 },
+                { id: 'bindings-transform', name: 'transform', visible: () => activeVersion >= 1.2 },
             ]
         },
+        { id: 'attributes', name: 'Attributes', icon: 'fas fa-at' },
         {
-            id: 'features',
             name: 'Features',
             icon: 'fas fa-cogs',
             children: [
                 { id: 'features-interaction', name: 'Interactivity' },
                 { id: 'features-reactivity', name: 'Reactivity' },
                 { id: 'features-custom', name: 'Custom elements', visible: () => activeVersion >= 2.2 },
-                { id: 'features-sections', name: 'Sections', visible: () => activeVersion >= 3.3 },
                 { id: 'features-pow-safe', name: 'pow.safe', visible: () => activeVersion >= 1.1 },
             ]
         },
@@ -88,26 +70,40 @@ const nav = {
         // },
         { id: 'breaking-changes', name: 'Breaking changes', icon: 'fas fa-exclamation-triangle' },
         { id: 'troubleshooting', name: 'Troubleshooting', icon: 'fas fa-bug' },
-        { id: 'repo', name: 'Repository', url: 'https://github.com/IFYates/pow.js', icon: 'fab fa-github' },
+        { name: 'Repository', url: 'https://github.com/IFYates/pow.js', icon: 'fab fa-github' },
     ]
 }
 
 window.activeVersion = nav.versions[0]
 
+const mainBinding = pow.bind(document.getElementsByTagName('main')[0])
 const navBinding = pow.apply(document.getElementsByTagName('nav')[0], nav)
 pow.apply(document.getElementById('preloader'), nav)
 HTMLImportElement.whenInitialised(() => {
     mainBinding.apply(nav)
-    if (location.hash)  {
-        const value = location.hash
-        location.hash = ''
-        location.hash = value
-    }
+    const value = location.hash || nav.current
+    location.hash = ''
+    location.hash = value
 })
 
 window.setActiveVersion = function (context) {
     window.activeVersion = context.$data
-    window.refreshMain()
+    mainBinding.refresh()
     navBinding.refresh()
 }
 window.version = (firstVersion, lastVersion) => activeVersion >= firstVersion && (!lastVersion || activeVersion < lastVersion)
+
+// React to user scrolling
+let lastPage = null
+window.addEventListener('scroll', () => {
+    const pages = [...document.querySelectorAll('main div.page')]
+    const topPage = pages.map($ => ({ id: $.id, pos: Math.min(Math.abs($.offsetTop - window.scrollY), Math.abs($.offsetTop + ($.offsetHeight * 0.5) - window.scrollY)) }))
+        .reduce((a, b) => a.pos < b.pos ? a : b)
+
+    if (lastPage != topPage?.id) {
+        lastPage = topPage?.id
+        for (const nav of document.querySelectorAll('.sidebar-link')) {
+            nav.classList.toggle('active', nav.id.slice(4) == lastPage)
+        }
+    }
+})
